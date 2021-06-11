fe7bb6db1f61cce107cf262b7690361bb49b2140
Theo Paris <theoparisdesigns@gmail.com>
2021-06-11 16:46:34 -0700

    - Fixed eslint workflow - Added husky precommit hook

--- packages/frontend/src/components/header.tsx
+++ packages/frontend/src/components/header.tsx
@@ -35,9 +35,8 @@ export const Header = () => {
         storage.onChanged.addListener(handleStorage);
 
         return () => {
-            if (storage.onChanged.hasListener(handleStorage)) {
+            if (storage.onChanged.hasListener(handleStorage))
                 storage.onChanged.removeListener(handleStorage);
-            }
         };
     }, [token]);
 
@@ -65,7 +64,7 @@ export const Header = () => {
             >
                 <Menu>
                     <MenuButton>
-                        <Avatar size="md" alt="Links" mb="0.5em"/>
+                        <Avatar size="md" alt="Links" mb="0.5em" />
                     </MenuButton>
                     <MenuList>
                         {token && (
